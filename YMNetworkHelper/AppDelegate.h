28c7acb0aa5fdad4dec78e59d233088ba42a407e
Alen <alen@alendemacbook-pro.local>
2016-04-07 21:48:19 +0800

    Initial Commit

---
+++ YMNetworkHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YMNetworkHelper
+//
+//  Created by Alen on 16/4/7.
+//  Copyright © 2016年 JingKeCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YMNetworkHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YMNetworkHelper
+//
+//  Created by Alen on 16/4/7.
+//  Copyright © 2016年 JingKeCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
